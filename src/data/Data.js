1d46cc92cb5154e28f4061e67d08873c14773a03
Ketaki56 <bhopepranita@gmail.com>
2022-09-06 13:40:01 +0530

    third commit

---
+++ src/data/Data.js
@@ -0,0 +1,177 @@
+export const Data=[
+    {
+    id: 1,
+    name: 'Double Cheeseburger',
+    category: 'burger',
+    image: 'https://images.pexels.com/photos/13163534/pexels-photo-13163534.jpeg?auto=compress&cs=tinysrgb&w=600',
+    price: '$'
+    },
+
+    {
+        id: 2,
+        name: 'Bacon Cheeseburger',
+        category: 'burger',
+        image:'https://images.pexels.com/photos/1639557/pexels-photo-1639557.jpeg?auto=compress&cs=tinysrgb&w=600', 
+        price: '$$'
+        },
+
+        {
+            id: 3,
+            name: 'Mushroom Burger',
+            category: 'burger',
+            image:'https://images.pexels.com/photos/12031039/pexels-photo-12031039.jpeg?auto=compress&cs=tinysrgb&w=600', 
+            price: '$$$'
+            },
+
+
+            {
+                id: 4,
+                name: 'Bacon Cheeseroll',
+                category: 'burger',
+                image:'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&w=600', 
+                price: '$$$$'
+                },
+
+                {
+                    id: 5,
+                    name: 'Cheese Pizza',
+                    category: 'Pizza',
+                    image:'https://images.pexels.com/photos/4553111/pexels-photo-4553111.jpeg?auto=compress&cs=tinysrgb&w=600', 
+                    price: '$$'
+                    },
+
+                    {
+                        id: 6,
+                        name: 'Green Pizza',
+                        category: 'pizza',
+                        image:'https://images.pexels.com/photos/1146760/pexels-photo-1146760.jpeg?auto=compress&cs=tinysrgb&w=600', 
+                        price: '$'
+                        },
+
+                        {
+                            id: 7,
+                            name: 'Red Pizza',
+                            category: 'pizza',
+                            image:'https://images.pexels.com/photos/8444553/pexels-photo-8444553.jpeg?auto=compress&cs=tinysrgb&w=600', 
+                            price: '$$$'
+                            },
+
+                            {
+                                id: 8,
+                                name: 'Tripple Pizza',
+                                category: 'pizza',
+                                image:'https://images.pexels.com/photos/1566837/pexels-photo-1566837.jpeg?auto=compress&cs=tinysrgb&w=600', 
+                                price: '$$$$'
+                                },
+
+
+
+                        {
+                            id: 9,
+                            name: 'Creamy Chicken',
+                            category: 'Chicken',
+                            image:'https://images.pexels.com/photos/2338407/pexels-photo-2338407.jpeg?auto=compress&cs=tinysrgb&w=600', 
+                            price: '$'
+                            },
+
+                            {
+                                id: 10,
+                                category: 'chicken',
+                                name: 'Chicken Fry',
+                                image:'https://images.pexels.com/photos/2673353/pexels-photo-2673353.jpeg?auto=compress&cs=tinysrgb&w=600', 
+                                price: '$$$'
+                                },
+
+                                {
+                                    id: 11,
+                                    category: 'chicken',
+                                    name: 'Fish Fry',
+                                    image:'https://images.pexels.com/photos/1352270/pexels-photo-1352270.jpeg?auto=compress&cs=tinysrgb&w=600', 
+                                    price: '$$$$'
+                                    },
+
+                                    {
+                                        id: 12,
+                                        category: 'chicken',
+                                        name: 'Spicy Chicken',
+                                        image:'https://images.pexels.com/photos/2232433/pexels-photo-2232433.jpeg?auto=compress&cs=tinysrgb&w=600', 
+                                        price: '$$'
+                                        },
+
+
+
+                                {
+                                    id: 13,
+                                    category: 'salad',
+                                    name: 'salad 65',
+                                    image:'https://images.pexels.com/photos/9328489/pexels-photo-9328489.jpeg?auto=compress&cs=tinysrgb&w=600', 
+                                    price: '$'
+                                    },
+
+                                    {
+                                        id: 14,
+                                        category: 'salad',
+                                        name: 'Cheese Salad',
+                                        image:'https://images.pexels.com/photos/128388/pexels-photo-128388.jpeg?auto=compress&cs=tinysrgb&w=600', 
+                                        price: '$$$'
+                                        },
+
+                                        {
+                                            id: 15,
+                                            category: 'salad',
+                                            name: 'Chicken Salad',
+                                            image:'https://images.pexels.com/photos/434258/pexels-photo-434258.jpeg?auto=compress&cs=tinysrgb&w=600', 
+                                            price: '$$'
+                                            },
+
+                                            {
+                                                id: 16,
+                                                category: 'salad',
+                                                name: 'Chicken cheese Salad',
+                                                image:'https://images.pexels.com/photos/725991/pexels-photo-725991.jpeg?auto=compress&cs=tinysrgb&w=600', 
+                                                price: '$$$$'
+                                                },
+    
+
+];
+
+export const categories=[
+    {
+        id:1,
+        name:'Fast Food',
+        image:'https://img.icons8.com/external-smashingstocks-circular-smashing-stocks/2x/external-fast-food-independence-day-usa-smashingstocks-circular-smashing-stocks.png'
+    },
+
+    {
+        id:2,
+        name:'Pizza',
+        image: 'https://img.icons8.com/color/72/pizza-five-eighths.png'
+    },
+
+    {
+        id:3,
+        name:'Wings',
+        image: 'https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/2x/external-chicken-wings-fast-food-vitaliy-gorbachev-lineal-color-vitaly-gorbachev.png'
+    },
+
+    {
+        id:4,
+        name:'Latest Deals',
+        image:'https://img.icons8.com/external-filled-outline-wichaiwi/344/external-deal-business-model-canvas-filled-outline-wichaiwi.png'
+    },
+
+    {
+        id:5,
+        name:'Restaurant Rewards',
+        image:'https://img.icons8.com/external-flaticons-flat-flat-icons/2x/external-rewards-cyber-monday-flaticons-flat-flat-icons.png'
+    },
+
+    {
+        id:6,
+        name:'Best Overall',
+        image:'https://img.icons8.com/fluency/2x/best-seller.png'
+    },
+
+
+
+]
